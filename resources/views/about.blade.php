af91792929db4ad59d76cfa35785ac4a84e92241
yassine-nid <nidkouchiyassine@gmail.com>
2022-04-23 13:45:24 +0000

    home page && about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,43 @@
 @extends('layout')
 @section('content')
-<h1>about</h1>
+    <!-- About Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
+                    <div class="position-relative h-100">
+                        <img class="img-fluid position-absolute w-100 h-100" src="{{asset('img/aboutimg.jpeg')}}" alt="" style="object-fit: cover;">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
+                    <h1 class="mb-4">Welcome to Key Academy</h1>
+                    <p class="mb-4">Whether you want to learn or to share what you know, you’ve come to the right place. As a global destination for online learning, we connect people through knowledge.</p>
+                    <p class="mb-4">Whatever your learning style, we have a course that fits. Coming from instructors all over the world, our courses cover just about anything you’d want to know.</p>
+                    <div class="row gy-2 gx-4 mb-4">
+                        <div class="col-sm-6">
+                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
+                        </div>
+                    </div>
+                    <a class="btn btn-primary py-3 px-5 mt-2" href="/contact">Contact us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
 @section('content')
-    <div class="bd-masthead mb-3" id="content">
+    <div class="bd-masthead mb-3 pt-5" id="content">
         <div class="container px-4 px-md-3">
             <div class="row align-items-lg-center">
                 <div class="col-8 mx-auto col-md-4 order-md-2 col-lg-5">
-                    <img src="https://i.pinimg.com/564x/da/d2/a3/dad2a36cccbe5497bf2fe8200be45b63.jpg" alt="" class="img-fluid mb-3 mb-md-0">
+                    <img src="{{asset('img/homeimg.jpeg')}}" alt="" class="img-fluid mb-3 mb-md-0">
                 </div>
                 <div class="col-md-8 order-md-1 col-lg-7 text-center text-md-start">
                     <h1 class="mb-3">Welcome to Key Academy best E-learning web site</h1>
@@ -14,7 +14,7 @@
 
                     <div class="d-flex flex-column flex-md-row">
                         <a href="/register" class="btn btn-lg btn-primary mb-3 me-md-3">Get started</a>
-                        <a href="" class="btn btn-lg btn-outline-secondary mb-3">Download</a>
+                        <a href="/login" class="btn btn-lg btn-outline-secondary mb-3">Sign In</a>
                     </div>
 
                 </div>
@@ -23,4 +23,3 @@
         </div>
     </div>
 @endsection
-
