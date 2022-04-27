ac4af338b772cf7ca67cd1d7f5b2386747c53333
yassine-nid <nidkouchiyassine@gmail.com>
2022-04-27 12:02:09 +0000

    create course

---
+++ database/migrations/2022_04_22_133019_create_cours_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cours', function (Blueprint $table) {
+            $table->id();$table->string("intitule");
+            $table->string("description")->nullable();
+            $table->string("duree");
+            $table->foreignId('formation_id')->constrained('formations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cours');
+    }
+};
