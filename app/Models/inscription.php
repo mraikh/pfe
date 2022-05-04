c31334d65bf079c4fce83a25471407a49f322a35
Mehdi Jai <mehdi.jai.mj@gmail.com>
2022-05-04 00:52:43 +0000

    Remove relations with Apprenant & Formation

--- app/Models/inscription.php
+++ app/Models/inscription.php
@@ -10,12 +10,4 @@ class inscription extends Model
     use HasFactory;protected $table = 'inscriptions';
     protected $fillable = ['formation_id', 'apprenant_id'];
     protected $increment = true;
-    public function Apprenant()
-    {
-        return $this->belongsTo('App\Models\Apprenant');
-    }
-    public function formation()
-    {
-        return $this->belongsTo('App\Models\formation');
-    }
 }
