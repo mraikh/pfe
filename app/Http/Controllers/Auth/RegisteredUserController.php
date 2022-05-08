4b9e573b68d048a1c8376c0309ca2ebabd378c7f
mraikh <nouhailamraikh@gmail.com>
2022-05-08 18:31:01 +0100

    tatata

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,7 +35,7 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        Log::info($request->all());
+
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
