9b8dfcec4eed7a19b125cb2580107dbe72c506a9
mraikh <nouhailamraikh@gmail.com>
2022-05-18 17:11:42 +0100

    edite profile

--- resources/views/formateur/profile.blade.php
+++ resources/views/formateur/profile.blade.php
@@ -11,8 +11,13 @@
             <div class="col-lg-4">
                 <div class="card mb-4">
                     <div class="card-body text-center">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
-                             class="rounded-circle img-fluid" style="width: 150px;">
+                    @if (!$formateur->user->photo)
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+                    class="rounded-circle img-fluid" style="width: 150px;">
+                    @else
+                    <img src="{{asset('storage/'.$formateur->user->photo)}}"  class="rounded-circle img-fluid" style="width: 150px;">
+                    @endif
+
                         <h5 class="my-3">{{$formateur->name}}</h5>
 {{--                        <p class="text-muted mb-1">{{$formateur->specialite}}</p>--}}
                         <p class="text-muted mb-4">{{$formateur->biography}}</p>
